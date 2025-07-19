b09f42f7a192db76adceabe97db36e3da6b3c7f4
Mauricio Loayza <mauricioloayzas@mauloasan.com>
2025-07-18 21:41:39 -0500

    commit configuration to execute test in a re-commit

--- libs/database/src/database.service.spec.ts
+++ libs/database/src/database.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseService } from './database.service';
+import { ConfigService } from '@nestjs/config'; // <-- Importa ConfigService
 
 describe('DatabaseService', () => {
   let service: DatabaseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DatabaseService],
+      providers: [
+        DatabaseService,
+        // Añade un mock de ConfigService para satisfacer la dependencia del constructor
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              // Devuelve valores de prueba si es necesario para el constructor
+              if (key === 'AWS_REGION') {
+                return 'us-east-1';
+              }
+              return null;
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<DatabaseService>(DatabaseService);
